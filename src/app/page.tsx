4da203ab3a3ff20338eb973c66dc9710a51c2b37
den.leonchev@gmal.com <den.leonchev@gmail.com>
2025-10-27 09:41:31 +0000

    Add about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,6 +14,15 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+          <nav className="mt-2">
+            <ul>
+              <li className="flex content-stretch hover:bg-gray-700 duration-200 rounded-lg">
+                <Link className="p-4" href="/about">
+                  About
+                </Link>
+              </li>
+            </ul>
+          </nav>
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
           </h1>
